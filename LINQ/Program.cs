92607fbd1eb72dfb2f42e3137ec1b1f0cce3e3ad
Max Schlapfer <max.schlapfer@gmail.com>
2022-10-08 23:48:32 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gameNames = new List<string>() { "Counter-Strike", "League of Legends", "Runescape", "Melee", "osu!", "Super Mario", "Clash Royale" };
+            Console.WriteLine("Original List: ");
+            
+            foreach(string gameName in gameNames)
+            {
+                Console.WriteLine(gameName);
+            }
+            
+            IEnumerable<string> orderedNames = gameNames.OrderBy(x => x.Length);
+            Console.WriteLine("\nOrdered List by Length: ");
+           
+            foreach(string gameName in orderedNames)
+            {
+                Console.WriteLine(gameName);
+            }
         }
     }
 }
